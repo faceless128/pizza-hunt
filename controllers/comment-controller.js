503c07bc1252dec96a6f6ee725c4da86870b5ebd
Kaijam Jeffers <faceless128@gmail.com>
2022-06-04 16:24:12 -0400

    added comment controllers

---
+++ controllers/comment-controller.js
@@ -0,0 +1,49 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+    // add comments to pizza
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+        .then(({ _id }) => {
+            return Pizza.findOneAndUpdate(
+                { _id: params.pizzaId },
+                { $push: { comments: _id } },
+                { new: true }
+            )
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // remove comment
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+        .then(deletedComment => {
+            if (!deletedComment) {
+                return res.status(404).json({ message: 'No comment with this id!' });
+            }
+            return Pizza.findOneAndUpdate(
+                { _id: PageTransitionEvent.pizzaId },
+                { $pull: {comments: params.commentId } },
+                { new: true }
+            )
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with id' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    }
+};
+
+modile.exports = commentController; 
