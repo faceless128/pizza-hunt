47b28869772eb7194256bd9c72b183c70557757e
Kaijam Jeffers <faceless128@gmail.com>
2022-06-05 15:31:55 -0400

    working on offline saving of data

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -11,22 +11,36 @@ request.onupgradeneeded = function(event) {
     // save a reference to the database
     const db = event.target.result;
     // create an object store (table) called 'new_pizza', set it to have an auto incrementing primary key of sorts
-    db.createObjectStore('new_pizza', { autoincrement: true });
+    db.createObjectStore('new_pizza', { autoIncrement: true });
+}
 
-    // upon a successful interaction
-    request.onsuccess = function(event) {
-        // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable
-        db = event.target.result
+// upon a successful interaction
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
 
-        // check if app is online, if yes, run uploadPizza() function to send all local db daya to api
-        if (navigator.onLine) {
-            // we haven't created this yet, but we will soon, so let's commment it out for noe
-            // uploadPizza();
-        }
-    };
-    
-    request.onerror = function(event) {
-        // log error here
-        console.log(event.target.errorCode);
+    // check if app is online, if yes, run uploadPizza() function to send all local db daya to api
+    if (navigator.onLine) {
+        // we haven't created this yet, but we will soon, so let's commment it out for noe
+        // uploadPizza();
     }
 };
+    
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+}
+
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+
+function saveRecord(record) {
+    // open a new tranaction with the databade with read and write permissions
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access the object store for 'new_pizza'
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // add record to your store wih add method
+    pizzaObjectStore.add(record);
+}
